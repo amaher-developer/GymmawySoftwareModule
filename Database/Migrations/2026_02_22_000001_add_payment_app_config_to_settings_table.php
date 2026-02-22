ea048aaa031903769cc6bdc33029eae1130acc90
maher <amaher.developer@gmail.com>
2026-02-22 13:47:37 +0200

    chore: delete obsolete migration for payments app configuration
    - Removed the migration file that added 'payments' and 'app_config' columns to the settings table, as it is no longer necessary.

--- Database/Migrations/2026_02_22_000001_add_payment_app_config_to_settings_table.php
+++ Database/Migrations/2026_02_22_000001_add_payment_app_config_to_settings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddPaymentsAppConfigToSettings extends Migration
+class AddPaymentAppConfigToSettingsTable  extends Migration
 {
     public function up(): void
     {
