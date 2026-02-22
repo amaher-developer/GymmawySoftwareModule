0e58e9dadc5fba89060333793edf2334a408af70
maher <amaher.developer@gmail.com>
2026-02-22 13:33:41 +0200

    fix: correct class name typo in migration file
    - Fixed a typo in the class name from 'AddPaymentAppConfigToSettins' to 'AddPaymentAppConfigToSettings' in the migration file for better clarity and consistency.

--- Database/Migrations/2026_02_22_000001_add_payment_app_config_to_settings_table.php
+++ Database/Migrations/2026_02_22_000001_add_payment_app_config_to_settings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddPaymentAppConfigToSettins extends Migration
+class AddPaymentAppConfigToSettings extends Migration
 {
     public function up(): void
     {
