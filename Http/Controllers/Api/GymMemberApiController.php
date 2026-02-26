7fe5a3228106c437eacaa301b67cd89d83fab80f
maher <amaher.developer@gmail.com>
2026-02-26 15:47:25 +0200

    refactor: reduce member retrieval limits in GymMemberApiController
    - Decreased the limit for fetching active and set gym members from 5000 to 50 to optimize performance and reduce data load.
    - Adjusted the limit for retrieving new members from 100 to 1, ensuring more precise data handling for new entries.
    - These changes aim to enhance the efficiency of data retrieval processes within the application.

--- Http/Controllers/Api/GymMemberApiController.php
+++ Http/Controllers/Api/GymMemberApiController.php
@@ -156,7 +156,7 @@ class GymMemberApiController extends GenericApiController
             ->whereIn('fp_check', [TypeConstants::ZK_ACTIVE_MEMBER, TypeConstants::ZK_SET_MEMBER])
             //->where('fp_check', TypeConstants::ZK_SET_MEMBER)
             ->orderBy('updated_at', 'desc')
-            ->limit(5000)
+            ->limit(50)
             ->get();
 
         $set_members_ids = $set_members->pluck('id');
@@ -170,18 +170,18 @@ class GymMemberApiController extends GenericApiController
             $del_members = GymMember::where('branch_setting_id',$branch_id)->whereNotIn('fp_id', $set_members_ids)->whereNotNull('fp_id')->where('fp_check_count', '<', 5)->where('fp_check', TypeConstants::ZK_EXPIRE_MEMBER)->withTrashed()->orderBy('id', 'asc')->pluck('fp_id');
 
         $set_new_members = GymMember::select('id','name', 'phone','fp_id', 'fp_uid', 'code')->with('member_subscription_info')->where('branch_setting_id',$branch_id)
-            ->whereNull('fp_id')->limit(100)->orderBy('id', 'desc')->get();
+            ->whereNull('fp_id')->limit(1)->orderBy('id', 'desc')->get();
         $set_new_members = ZKNewMemberResource::collection($set_new_members);
 
         $get_members = GymMember::where('branch_setting_id',$branch_id)->whereNotNull('fp_id')
             ->where('fp_check_count', '<', 10)
             ->whereIn('fp_check', [TypeConstants::ZK_ACTIVE_MEMBER, TypeConstants::ZK_SET_MEMBER])
             ->orderBy('updated_at', 'desc')
-            ->limit(5000)->pluck('fp_id')->toArray();
+            ->limit(50)->pluck('fp_id')->toArray();
         $get_users = GymUser::where('branch_setting_id',$branch_id)->whereNotNull('fp_id')->where('fp_check_count', '<', 10)
             ->whereIn('fp_check', [TypeConstants::ZK_ACTIVE_MEMBER, TypeConstants::ZK_SET_MEMBER])
             ->orderBy('updated_at', 'desc')
-            ->limit(5000)->pluck('fp_id')->toArray();
+            ->limit(50)->pluck('fp_id')->toArray();
 
         if(count($get_users) > 0)
             $get_members = array_merge($get_members, $get_users);
