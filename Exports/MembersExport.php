e3f58bc6a5ffbb47cfb2f8b7c0a7b036cbac4d4b
maher <amaher.developer@gmail.com>
2026-08-01 12:17:27 +0300

    ff

--- Exports/MembersExport.php
+++ Exports/MembersExport.php
@@ -25,7 +25,6 @@ class MembersExport implements FromCollection, WithHeadings, WithMapping, WithSt
 
     public function __construct($data)
     {
-        var_dump($data);
         $this->lang = $data['lang'];
         $this->data = $data['records'];
         $this->keys = $data['keys'];
