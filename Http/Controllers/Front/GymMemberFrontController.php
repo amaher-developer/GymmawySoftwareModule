b99635dec2524374d4e74b3a24ea13f745d197cc
maher <amaher.developer@gmail.com>
2026-08-01 12:09:56 +0300

    ff

--- Http/Controllers/Front/GymMemberFrontController.php
+++ Http/Controllers/Front/GymMemberFrontController.php
@@ -417,27 +417,12 @@ class GymMemberFrontController extends GymGenericFrontController
         $this->limit = request('limits') ?? 600;
         $records = $this->index()->with(\request()->all());
         $records = $records->members;
-        if ($records instanceof \Illuminate\Contracts\Pagination\Paginator) {
-            $records = $records->getCollection();
-        }
 
         //        $records = $this->MemberRepository->with(['member_subscription_info.subscription'])->get();
         $this->fileName = 'members-' . Carbon::now()->toDateTimeString();
 
-        foreach ($records as $key => $record) {
-            $records[$key]['barcode']          = $record['code'];
-            $records[$key]['phone']            = str_replace('+', '00', $record['phone'] ?? '');
-            $records[$key]['membership']       = $record['member_subscription_info']['subscription']['name'] ?? null;
-            $records[$key]['workouts']         = $record['member_subscription_info']['workouts'] ?? null;
-            $records[$key]['number_of_visits'] = $record['member_subscription_info']['visits'] ?? null;
-            $records[$key]['amount_remaining'] = $record['member_subscription_info']['amount_remaining'] ?? null;
-            $records[$key]['store_balance']    = $record['store_balance'] ?? null;
-            $records[$key]['joining_date']     = $record['member_subscription_info']['joining_date']
-                ? Carbon::parse($record['member_subscription_info']['joining_date'])->toDateString() : null;
-            $records[$key]['expire_date']      = $record['member_subscription_info']['expire_date']
-                ? Carbon::parse($record['member_subscription_info']['expire_date'])->toDateString() : null;
-            $records[$key]['status']           = $record['member_subscription_info']['status_name'] ?? null;
-        }
+//        $title = trans('sw.subscribed_clients');
+//        $records = $this->prepareForExport($records);
 
         $notes = trans('sw.export_excel_members');
         $this->userLog($notes, TypeConstants::ExportMemberExcel);
